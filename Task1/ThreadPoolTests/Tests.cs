5537f9fdb31b712aaa2074031f144dbbfc11bda3
VladimirMaltsev <vov.maltsev@gmail.com>
2020-11-23 14:55:59 +0300

    restruct folders, init tests

---
+++ Task1/ThreadPoolTests/Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1.ThreadPoolTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private const int threadPoolSize = 5;
+
+        [Test]
+        public void AddOneTaskTest()
+        {
+            using (var threadPool = new ThreadPool(threadPoolSize))
+            {
+                using (var task = new MyTask<int>(() => 42 + 42))
+                {
+                    threadPool.Enqueue(task);
+                    Assert.AreEqual(84, task.Result);
+                }
+            }
+        }
+
+        
+    }
+}
