a3b553db3811469e2413a8bd80692d3a7ae80d81
VladimirMaltsev <vov.maltsev@gmail.com>
2020-11-23 16:04:40 +0300

    creat ThreadPool

---
+++ Task1/ThreadPool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class ThreadPool : IDisposable
+    {
+        private volatile int _size = 10;
+
+        public int Size => _size;
+
+        public ThreadPool(int poolSize)
+        {
+            _size = poolSize;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
