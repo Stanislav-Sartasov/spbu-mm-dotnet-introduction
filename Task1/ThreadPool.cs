08aa1d72266baee2a9f9dcd241f551437226ff20
VladimirMaltsev <vov.maltsev@gmail.com>
2020-11-23 16:36:58 +0300

    construct pool

--- Task1/ThreadPool.cs
+++ Task1/ThreadPool.cs
@@ -1,22 +1,62 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Task1
 {
     class ThreadPool : IDisposable
     {
-        private volatile int _size = 10;
+        private const int _defaultSize = 10;
+        private readonly List<Thread> _pool;
+       
+        private readonly BlockingCollection<Action> _waitingTasks = new BlockingCollection<Action>();
+        public int Size { get; }
 
-        public int Size => _size;
-
-        public ThreadPool(int poolSize)
+        public ThreadPool(int size)
         {
-            _size = poolSize;
+            if (size <= 0)
+            {
+                throw new ArgumentException("Invalid ThreadPool size", nameof(size));
+            }
+            Size = size;
+            _pool = new List<Thread>(size);
+
+            for (var i = 0; i < size; i++)
+            {
+                var worker = new Thread(Work);
+                worker.Start();
+                _pool.Add(worker);
+            }
+        }
+
+        public ThreadPool() : this(_defaultSize)
+        {
         }
         public void Dispose()
         {
             throw new NotImplementedException();
+        }
+
+        private void Work()
+        {
+            while (true)
+            {
+                try
+                {
+                    var task = _waitingTasks.Take();
+                    task.Invoke();
+                }
+                catch (InvalidOperationException e)
+                {
+                    return;
+                }
+                catch (Exception)
+                {
+                    //do nothing
+                }
+            }
         }
     }
 }
