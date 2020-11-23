a67a9316ff2feecdfa8466d3c06e20f464352071
VladimirMaltsev <vov.maltsev@gmail.com>
2020-11-23 18:56:12 +0300

    add tests

--- Task1/ThreadPoolTests/Tests.cs
+++ Task1/ThreadPoolTests/Tests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Task1.ThreadPoolTests
 {
@@ -15,11 +16,69 @@ namespace Task1.ThreadPoolTests
         {
             using (var threadPool = new ThreadPool(threadPoolSize))
             {
-                using (var task = new MyTask<int>(() => 42 + 42))
+                using (var task = new MyTask<int>(() => 
+                {
+                    Thread.Sleep(1000);
+                    return 42 + 42;
+                }))
                 {
                     threadPool.Enqueue(task);
                     Assert.AreEqual(84, task.Result);
                 }
+            }
+        }
+
+        [Test]
+        public void AddManyTasksCheckCountTest()
+        {
+            using (var threadPool = new ThreadPool(threadPoolSize))
+            {
+                using (var task1 = new MyTask<int>(() =>
+                {
+                    Thread.Sleep(1000);
+                    return 42 + 42;
+                }))
+                using (var task2 = new MyTask<int>(() =>
+                {
+                    Thread.Sleep(1000);
+                    return 42 + 42;
+                }))
+                using (var task3 = new MyTask<int>(() =>
+                {
+                    Thread.Sleep(1000);
+                    return 42 + 42;
+                }))
+                {
+                    threadPool.Enqueue(task1);
+                    threadPool.Enqueue(task2);
+                    threadPool.Enqueue(task3);
+                    Assert.AreEqual(threadPoolSize, threadPool.Size);
+                }
+            }
+
+        }
+
+        [Test]
+        public void AddTasksAndDisposeThreadPoolTest()
+        {
+            using (var threadPool = new ThreadPool(threadPoolSize))
+            {
+                var task1 = new MyTask<int>(() =>
+                {
+                    Thread.Sleep(1000);
+                    return 42 + 42;
+                });
+                var task2 = new MyTask<int>(() =>
+                {
+                    Thread.Sleep(1000);
+                    return 43 + 41;
+                });
+                threadPool.Enqueue(task1);
+                threadPool.Enqueue(task2);
+                Assert.AreEqual(84, task1.Result);
+                Assert.AreEqual(84, task2.Result);
+                task1.Dispose();
+                task2.Dispose();
             }
         }
 
