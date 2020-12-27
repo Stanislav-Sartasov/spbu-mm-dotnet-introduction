7deead63b0af0c4933c0fb97c4bcc7fbb9321437
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-12-27 22:28:21 +0300

    Finish Task3

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -1,47 +1,87 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
 
 namespace Homework
 {
-  internal class Program
-  {
-    public static void Main()
+    internal static class Program
     {
-      var a = new A
-      {
-        B = new B
+        public static void Main()
         {
-          C = new C
-          {
-            Value = "The meaning of life, universe and everything"
-          }
+            var a = new A
+            {
+                B = new B
+                {
+                    C = new C
+                    {
+                        Value = "42"
+                    }
+                }
+            };
+
+            var anotherA = new A
+            {
+                B = new B()
+            };
+
+            var access = Create<A, string>(new []{"B", "C", "Value"});
+
+            Console.WriteLine($"The meaning of life, universe and everything is {access(a)}");
+            Console.WriteLine($"My good New Year spirit is {access(anotherA) ?? "<null>"}");
         }
-      };
 
-      var anotherA = new A
-      {
-        B = new B()
-      };
-    }
+        [NotNull]
+        private static Func<TRoot, TResult> Create<TRoot, TResult>([NotNull] IEnumerable<string> properties)
+        {
+            var parameter = Expression.Parameter(typeof(TRoot));
+            var exit = Expression.Label(typeof(TResult));
 
-    private object Access(object root, IEnumerable<string> properties)
-    {
-      
-    }
+            var locals = new List<ParameterExpression>();
+            var expressions = new List<Expression>();
 
-    private class A
-    {
-      public B B { get; set; }
-    }
+            var currentVariable = parameter;
+            var currentType = typeof(TRoot);
+            foreach (string propertyName in properties)
+            {
+                var field = currentType.GetField(propertyName).NotNull();
+                var nextVariable = Expression.Variable(field.FieldType);
+                locals.Add(nextVariable);
+                expressions.Add(Expression.IfThenElse(
+                    Expression.NotEqual(currentVariable, Expression.Constant(null)),
+                    Expression.Assign(nextVariable, Expression.Field(currentVariable, field)),
+                    Expression.Return(exit, Expression.Constant(null, typeof(TResult)))
+                ));
 
-    private class B
-    {
-      public C C { get; set; }
-    }
+                currentVariable = nextVariable;
+                currentType = nextVariable.Type;
+            }
 
-    private class C
-    {
-      public string Value { get; set; }
+            expressions.Add(Expression.Return(exit, currentVariable));
+            expressions.Add(Expression.Label(exit, Expression.Constant(null, typeof(TResult))));
+            return Expression.Lambda<Func<TRoot, TResult>>(Expression.Block(locals, expressions), parameter).Compile();
+        }
+
+        private class A
+        {
+            public B B;
+        }
+
+        private class B
+        {
+            public C C;
+        }
+
+        private class C
+        {
+            public string Value;
+        }
+
+        [NotNull]
+        public static T NotNull<T>([CanBeNull] this T value) where T : class
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            return value;
+        }
     }
-  }
 }
